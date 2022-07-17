6aadc79add8ea2efb2aa58692a665917921eb74c
clocwerk <65974401+clocwerk@users.noreply.github.com>
2022-07-18 02:52:54 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const items = galleryItems.map(({ preview, original, description }) => {
+  return `<div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+});
+
+gallery.insertAdjacentHTML("beforeend", items.join(""));
+
+gallery.addEventListener("click", handerGalleryClick);
+
+let modalEl;
+
+function handerGalleryClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  modalEl = basicLightbox.create(
+    `<img src="${event.target.dataset.source}"/>`,
+    {
+      onShow: () => {
+        document.addEventListener("keydown", closeModalEsc);
+      },
+      onClose: () => {
+        document.removeEventListener("keydown", closeModalEsc);
+      },
+    }
+  );
+
+  modalEl.show();
+}
+
+function closeModalEsc(event) {
+  if (event.code !== "Escape") {
+    return;
+  }
+  modalEl.close();
+  // console.log(event.code);
+}
